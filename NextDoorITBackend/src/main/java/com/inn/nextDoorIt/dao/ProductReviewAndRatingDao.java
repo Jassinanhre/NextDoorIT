bbe07ad01e0b520699792b47bccff34601fd8200
SHIVAY_SHARMA <sanwalrohit333@gmail.com>
2023-08-06 08:06:33 +0530

    Fixed ratings issue and product details issue

--- NextDoorITBackend/src/main/java/com/inn/nextDoorIt/dao/ProductReviewAndRatingDao.java
+++ NextDoorITBackend/src/main/java/com/inn/nextDoorIt/dao/ProductReviewAndRatingDao.java
@@ -3,5 +3,8 @@ package com.inn.nextDoorIt.dao;
 import com.inn.nextDoorIt.entity.ProductReviewAndRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductReviewAndRatingDao extends JpaRepository<ProductReviewAndRating, Integer> {
+    List<ProductReviewAndRating> findByProductId(int productId);
 }
