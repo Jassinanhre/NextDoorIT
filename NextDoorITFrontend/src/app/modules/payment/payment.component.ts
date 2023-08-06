dcbd3a93dbf8ac1201aae78b5b2174459032757e
dixit <dixitdreamer@gmail.com>
2023-08-06 20:42:15 +0530

    Added payment method

--- NextDoorITFrontend/src/app/modules/payment/payment.component.ts
+++ NextDoorITFrontend/src/app/modules/payment/payment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { OrderService } from 'src/app/services/user/order/order.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GlobalConstants } from 'src/app/global-constants';
 
 @Component({
   selector: 'app-payment',
@@ -12,14 +14,23 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 export class PaymentComponent implements OnInit {
   showReceipt: boolean = false;
   orderInfo: any = {};
+  paymentForm: any = FormGroup;
+
 
   constructor(
+    private formBuilder: FormBuilder,
     private orderService: OrderService,
     private localStorageService: LocalStorageService,
   ) { }
 
   ngOnInit(): void {
     this.getOrderInfo()
+    this.paymentForm = this.formBuilder.group({
+      method: [null, [Validators.required]],
+      cardNumber: [null, [Validators.required]],
+      expiry: [null, [Validators.required]],
+      cvv: [null, [Validators.required]],
+    })
   }
 
   getOrderInfo(): void {
@@ -32,6 +43,15 @@ export class PaymentComponent implements OnInit {
   }
 
   handlePay() {
+    const formData = this.paymentForm.value;
+    const payload: any = {
+      userId: this.localStorageService.getItem('userId'),
+      "method": formData.method,
+      "cardNumber": formData.cardNumber,
+      "expiry": formData.expiry,
+      "cvv": formData.cvv
+    }
+    console.log("payload : ", payload);
     this.showReceipt = !this.showReceipt;
   }
 
